815ac50dc96b661f14e08f26108b6f2955f8f94e
Henry Pahn <thoang0303@gmail.com>
2025-01-14 21:36:47 -0500

    Connect Client Web App to Secure Microservice

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // src/app.js
 
 import { signIn, getUser } from './auth';
+import { getUserFragments } from './api';
 
 async function init() {
   // Get our UI elements
@@ -15,9 +16,13 @@ async function init() {
 
   // See if we're signed in (i.e., we'll have a `user` object)
   const user = await getUser();
+  
   if (!user) {
     return;
   }
+
+  // Do an authenticated request to the fragments API server and log the result
+  const userFragments = await getUserFragments(user);
 
   // Update the UI to welcome the user
   userSection.hidden = false;
