b7585dbad239fd05cca788d44a486ffd92ca5bb6
HenryPahn <thoang0303@gmail.com>
2025-03-05 15:49:22 -0500

    Add function to get fragment metadata

--- src/api.js
+++ src/api.js
@@ -51,3 +51,26 @@ export async function createFragment(user, contentType, fragmentData) {
     console.error('Unable to call GET /v1/fragment', { err });
   }
 }
+
+
+export async function getFragmentMetaData(user, fragmentId) {
+  console.log(`Requesting user fragment with id: ${fragmentId}`);
+  try {
+    const res = await fetch(`${apiUrl}/v1/fragments/${fragmentId}/info`, {
+      // Generate headers with the proper Authorization bearer token to pass.
+      // We are using the `authorizationHeaders()` helper method we defined
+      // earlier, to automatically attach the user's ID token.
+      headers: {
+        ...user.authorizationHeaders(),
+      },
+    });
+    if (!res.ok) {
+      throw new Error(`${res.status} ${res.statusText}`);
+    }
+    const data = await res.json();
+    console.log('Successfully got user fragment metadata', { data });
+    return data;
+  } catch (err) {
+    console.error('Unable to call GET /v1/fragment', { err });
+  }
+}
